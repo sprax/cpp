bb331dc850b367c94bf86b4251a521d25d0b8db6
Sprax Lines <sprax@users.noreply.github.com>
2016-06-07 14:41:23 -0400

    Update and rename PrintOne.h to printOne.h

--- multi/MyPrint/printOne.h
+++ multi/MyPrint/printOne.h
@@ -1,14 +1,14 @@
 //
-//  PrintOne.h
+//  printOne.h
 //  MyPrint
 //
 //  Created by Sprax Lines on 6/6/16.
 //  Copyright © 2016 Sprax Lines. All rights reserved.
 //
 
-#ifndef PrintOne_h
-#define PrintOne_h
+#ifndef printOne_h
+#define printOne_h
 
 void printOne(const char *str);
 
-#endif /* PrintOne_h */
+#endif /* printOne_h */
