6eb83b88a62ac928acc8dcd576e3d04d3aabffab
Sprax Lines <sprax@talla.com>
2017-09-17 10:02:18 -0400

    string + char*

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,12 +1,13 @@
 // Compile and Run:
-// Mac osx: g++ hello.cpp -o hello.out && ./hello.out
+// Mac osx: g++ hello.cpp -o hello.out && hello.out  [you might need ./hello.out]
 //
 #include <iostream>
-//#include <string>
+#include <string>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  const char *name = argc > 1 ? argv[1] : "Nameless One";
+  const string usage = string(" [usage: ") + argv[0] + " name]";
+  const string name = argc > 1 ? argv[1] : string("Nameless One") + usage;
   cout << "This is " << argv[0] << ", just saying Hello, " << name << "."  << endl;
   return 0;
 }
