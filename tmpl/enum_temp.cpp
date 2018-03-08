df91c8fe10fc3b85207043d55e0a2f29d499ec54
Sprax Lines <sprax@talla.com>
2018-03-08 10:59:11 -0500

    pre- and post- c++11 std::vector braced-list-initializer

--- tmpl/enum_temp.cpp
+++ tmpl/enum_temp.cpp
@@ -8,10 +8,10 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <thread>
 #include <typeinfo>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 using std::cin;
 using std::cout;
@@ -45,10 +45,13 @@ typedef enum { blood, blue  } blister;
 typedef enum { naval, seedless, /*blood*/ } orange;  /* blood gets a redefinition error; previous def was 'enumerator' */
 
 typedef enum tagMotionType {
+    FIRST_MotionType    = 0,
     SCOOP_TYPE_1 		= 0,
     SCOOP_TYPE_2 		= 1,
-    NUM_MOTION_TYPES 	= 2
+    NUM_MotionTypes 	= 2
 } MotionType;
+
+
 
 template<MotionType M>
 class Mover
@@ -59,12 +62,44 @@ public:
     }
 };
 
+
+template<MotionType M>
+class SpecMover : public Mover<M>
+{
+public:
+    void showMover() {
+        cout << "Mover of MotionType " << M << endl;
+    }
+};
+
+
+void listMotionTypes()
+{
+    #if __cplusplus <= 199711L
+    static const int arr[] = {16,2,77,29};
+    std::vector<int> int_vec (arr, arr + sizeof(arr) / sizeof(arr[0]) );
+    static const MotionType mta[] = {SCOOP_TYPE_1, SCOOP_TYPE_2};
+    std::vector<MotionType> template_arg_vec (mta, mta + sizeof(mta) / sizeof(mta[0]) );
+    #else
+    // NOTE: g++ -std=c++11   OR   clang++ -std=c++11
+    std::vector <int> int_vec = {1, 2, 3, 4, 5};
+    std::vector<MotionType> template_arg_vec = {SCOOP_TYPE_1, SCOOP_TYPE_2};
+    #endif
+    for (int j = FIRST_MotionType; j < NUM_MotionTypes; j++) {
+        MotionType typer = template_arg_vec[j];
+        printf("MotionType(%d)\n", typer);
+        const int tt = static_cast<int>(typer);
+        Mover<SCOOP_TYPE_2> mover;
+        mover.showMover();
+    }
+}
+
+
 int main(int argc, char* argv[])    // NB: This is more a unit test than an app; it does not play ghost!
 {
     const unsigned millis = 2222;
     factorials();
-    Mover<SCOOP_TYPE_2> mover2;
-    mover2.showMover();
+    listMotionTypes();
 
     return 0;
 }
