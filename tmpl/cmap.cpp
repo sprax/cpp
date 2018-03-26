6b0e9dceb6df8e133756f331dc9df113a0c7da6e
Sprax Lines <sprax@talla.com>
2018-03-26 15:46:13 -0400

    showMap

--- tmpl/cmap.cpp
+++ tmpl/cmap.cpp
@@ -32,13 +32,13 @@ std::vector<TK> extract_keys(std::map<TK, TV> const& input_map) {
 }
 
 template <typename TV>
-class MapIt {
+class MapTraj {
 public:
-    static const unsigned num_keys = 3;
+    static const unsigned num_keys = 4;
     static const std::string map_keys[num_keys];
 
-    MapIt() {}
-    virtual ~MapIt() {}
+    MapTraj() {}
+    virtual ~MapTraj() {}
 
     const std::string* getKeys()
     {
@@ -46,12 +46,16 @@ public:
     }
 
     /**
-     * Sets the value of an existing key and returns true;
+     * Updates the value of an existing key and returns true;
      * else fails and returns false.
      */
-    bool setIt(const std::string& key, TV val)
+    bool updateTraj(const std::string& key, TV val)
     {
-        cmap[key] = val;
+        auto it = cmap.find(key);
+        if (it == cmap.end()) {
+            return false;
+        }
+        it->second = val;
         return true;
     }
 
@@ -59,36 +63,66 @@ public:
      * Sets the value of an existing key and returns true;
      * else fails and returns false.
      */
-    int getIt(const std::string& key)
+    TV getTraj(const std::string& key, const TV& default_value)
     {
-        return cmap[key];
+        auto it = cmap.find(key);
+        if (it == cmap.end()) {
+            return default_value;
+        }
+        return it->second;
     }
 
 
-    void show() {
-        cout << "Using keys array:" << endl;
-        for (auto key : map_keys) {
-            std::cout << key << " :: " << cmap.at(key) << std::endl;
+    void show(int verbose=1)
+    {
+        if (verbose > 0) {
+            cout << "Using keys array:" << endl;
+            for (auto key : map_keys) {
+                std::cout << key << " :: " << cmap.at(key) << std::endl;
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-        cout << "Using map entry it:" << endl;
-        for (auto entry : cmap) {
-            std::cout << entry.first << " :: " << entry.second << std::endl;
+        if (verbose > 1) {
+            std::cout << std::endl;
+            cout << "Using map entry it:" << endl;
+            for (auto entry : cmap) {
+                std::cout << entry.first << " :: " << entry.second << std::endl;
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
     }
 
-private:
+    void showMap() {
+        for (auto& entry: cmap) {
+            cout << "KEY: " << entry.first << " => " << entry.second << endl;
+        }
+    }
+
+public:
+    /**
+     * Sets the value of a new or existing key and returns true;
+     * else fails and returns false.
+     */
+    bool setTraj(const std::string& key, TV val)
+    {
+        cmap[key] = val;
+        return true;
+    }
+
+public:
+
     std::map<std::string, TV> cmap = {
         {map_keys[1], 1},
         {map_keys[0], 0},
+        {map_keys[3], 3},
         {map_keys[2], 2},
     };
 
 };
 
 template<>
-const std::string MapIt<int>::map_keys[MapIt::num_keys] = {
+const std::string MapTraj<int>::map_keys[MapTraj::num_keys] = {
+    "zero",
     "one",
     "two",
     "three"
@@ -97,7 +131,16 @@ const std::string MapIt<int>::map_keys[MapIt::num_keys] = {
 
 int main(int argc, char* argv[])    // NB: This is more a unit test than an app; it does not play ghost!
 {
-    MapIt<int> mapit;
+    MapTraj<int> mapit;
     mapit.show();
+    cout<< "updateTraj('three', 4) returns: " << mapit.updateTraj("three", 4) << "; now show:" << endl;
+    mapit.show();
+    cout<< "setTraj('four', 4) returns: " << mapit.setTraj("four", 4) << "; now show:" << endl;
+    mapit.show();
+    cout<< "getTraj('forty') returns: " << mapit.getTraj("forty", -9999) << "; now show:" << endl;
+    mapit.show();
+
+    mapit.showMap();
+
     return 0;
 }
