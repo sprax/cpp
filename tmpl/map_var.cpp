083521a88b31585eddb613a3035deb0c3f844465
Sprax Lines <sprax@talla.com>
2018-03-27 11:00:10 -0400

    vint_t

--- tmpl/map_var.cpp
+++ tmpl/map_var.cpp
@@ -80,7 +80,7 @@ public:
         if (verbose > 0) {
             cout << "Using keys array:" << endl;
             for (auto key : map_keys_) {
-                std::cout << key << " :: " << traj_map_.at(key) << std::endl;
+                std::cout << key << " :: " << traj_map_.at(key).size() << std::endl;
             }
             std::cout << std::endl;
         }
@@ -88,7 +88,7 @@ public:
             std::cout << std::endl;
             cout << "Using map entry it:" << endl;
             for (auto entry : traj_map_) {
-                std::cout << entry.first << " :: " << entry.second << std::endl;
+                std::cout << entry.first << " :: " << entry.second.size() << std::endl;
             }
             std::cout << std::endl;
         }
@@ -118,31 +118,51 @@ public:
 
 };
 
+typedef std::vector<int> vint_t;
 
-
-
-int main(int argc, char* argv[])    // NB: This is more a unit test than an app; it does not play ghost!
+class MapTrajVec : public MapTraj<vint_t>
 {
-    MapTraj<int> map_int(-1);
-    map_int.show();
-    cout<< "updateTraj('three', 4) returns: " << map_int.updateTraj("three", 4) << "; now show:" << endl;
-    map_int.show();
-    cout<< "setTraj('four', 4) returns: " << map_int.setTraj("four", 4) << "; now show:" << endl;
-    map_int.show();
-    cout<< "getTraj('forty') returns: " << map_int.getTraj("forty", -9999) << "; now show:" << endl;
-    map_int.show();
-    map_int.showMap();
+public:
+    MapTrajVec() : MapTraj(std::vector<int>())
+    {}
+
+};
 
 
-    MapTraj<std::string> map_str("DEF_STR");
-    map_str.show();
-    cout<< "updateTraj('three', 'FOUR') returns: " << map_str.updateTraj("three", "FOUR") << "; now show:" << endl;
-    map_str.show();
-    cout<< "setTraj('four', 'FOUR') returns: " << map_str.setTraj("four", "FOUR") << "; now show:" << endl;
-    map_str.show();
-    cout<< "getTraj('forty') returns: " << map_str.getTraj("forty", "NaN") << "; now show:" << endl;
-    map_str.show();
-    map_str.showMap();
+int main(int argc, char* argv[])    // NB: unit tests for MapTraj
+{
+    MapTrajVec map_vec;
+    map_vec.show();
+    vint_t vintA = {};
+    std::vector<int> vintB = {1,2,3};
+    cout<< "updateTraj('three', 4) returns: " << map_vec.updateTraj("three", vintA) << "; now show:" << endl;
+    map_vec.show();
+    // cout<< "setTraj('four', 4) returns: " << map_vec.setTraj("four", 4) << "; now show:" << endl;
+    // map_vec.show();
+    // cout<< "getTraj('forty') returns: " << map_vec.getTraj("forty", -9999) << "; now show:" << endl;
+    // map_vec.show();
+    // map_vec.showMap();
+
+    // MapTraj<int> map_int(-1);
+    // map_int.show();
+    // cout<< "updateTraj('three', 4) returns: " << map_int.updateTraj("three", 4) << "; now show:" << endl;
+    // map_int.show();
+    // cout<< "setTraj('four', 4) returns: " << map_int.setTraj("four", 4) << "; now show:" << endl;
+    // map_int.show();
+    // cout<< "getTraj('forty') returns: " << map_int.getTraj("forty", -9999) << "; now show:" << endl;
+    // map_int.show();
+    // map_int.showMap();
+    //
+    //
+    // MapTraj<std::string> map_str("DEF_STR");
+    // map_str.show();
+    // cout<< "updateTraj('three', 'FOUR') returns: " << map_str.updateTraj("three", "FOUR") << "; now show:" << endl;
+    // map_str.show();
+    // cout<< "setTraj('four', 'FOUR') returns: " << map_str.setTraj("four", "FOUR") << "; now show:" << endl;
+    // map_str.show();
+    // cout<< "getTraj('forty') returns: " << map_str.getTraj("forty", "NaN") << "; now show:" << endl;
+    // map_str.show();
+    // map_str.showMap();
 
 
     return 0;
