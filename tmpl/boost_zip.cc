5d2e17a840512b62de40db78e72fd7cbd0df393e
Sprax Lines <sprax@talla.com>
2018-04-16 07:11:21 +0200

    HAVE_REDI zipper

--- tmpl/boost_zip.cc
+++ tmpl/boost_zip.cc
@@ -3,6 +3,20 @@
 #include <list>
 #include <string>
 
+#ifdef HAVE_REDI
+#include <redi/zip.h>   // for a zip function which works with range-base for
+                        // and accepts any number of ranges, which can be
+                        // rvalues or lvalues and can be different lengths
+                        // (iteration will stop at the end of the shortest range).
+
+void redi_zip() {
+std::vector<int> vi{ 0, 2, 4 };
+std::vector<std::string> vs{ "1", "3", "5", "7" };
+for (auto i : redi::zip(vi, vs))
+    std::cout << i.get<0>() << ' ' << i.get<1>() << ' ';
+}
+#endif
+
 int main() {
     std::vector<int> a {4, 5, 6};
     double b[] = {7, 8, 9};
@@ -14,4 +28,7 @@ int main() {
         boost::tie(x, y, z, w) = tup;
         printf("%d %g %s %d\n", x, y, z.c_str(), w);
     }
+    #ifdef HAVE_REDI
+    redi_zip();
+    #endif  // HAVE_REDI
 }
