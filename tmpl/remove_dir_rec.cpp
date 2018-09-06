e7afe98dde9d53e432ee2856f078b2ee4e579afe
sprax <sprax@comcast.net>
2018-09-05 22:33:56 -0400

    remove_dir_rec.cpp

---
+++ tmpl/remove_dir_rec.cpp
@@ -0,0 +1,59 @@
+// remove_dir_rec.cpp.cpp
+// May require C11 or later
+// BUILD: clang++ remove_dir_rec.cpp -o tmpl && tmpl
+
+#include <chrono>
+#include <iostream>
+#include <time.h>
+
+
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/syslimits.h>
+#include <ftw.h>
+
+using std::cerr;
+using std::endl;
+
+
+/// call unlink or rmdir on the path, depending on the flag
+int rm_dir_or_file(const char *path, const struct stat *pStat, int flag, struct FTW *pFtw)
+{
+    int status = 0;
+    if (flag == FTW_DP) {
+        status = rmdir(path);
+        cerr << "status = rmdir(" << path << "); => " << status << endl;
+    } else {
+        status = unlink(path);
+        cerr << "status = unlink(" << path << "); => " << status << endl;
+    }
+    if (status != 0) {
+        perror(path);
+    } else {
+        cerr << "removed: " << path << endl;
+        // cerr << "visited: " << path << endl;
+    }
+    return status;
+}
+
+
+/// implements system("rm -rf")
+int remove_dir_rec(const char *dir_name)
+{
+    if (nftw(dir_name, rm_dir_or_file, OPEN_MAX, FTW_DEPTH)) {
+        perror(dir_name);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char* argv[])    // NB: unit tests for MapTraj
+{
+    const char *the_dir = argc > 1 ? argv[1] : "duh";
+    cerr << argv[0] << " " << the_dir << endl;
+    remove_dir_rec(the_dir);
+    return 0;
+}
