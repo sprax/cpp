1f700e29b3f4f027eacbf54a6d9d43a6405cc783
sprax <sprax@comcast.net>
2019-04-23 15:05:24 -0400

    warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]

--- tmpl/strpal.cpp
+++ tmpl/strpal.cpp
@@ -1,7 +1,7 @@
 // strpal.cpp : test if string is palindrome
 // Requires C11 or later
 // BUILD: clang++ -std=c++11 strpal.cpp -o tmpl.out && tmpl.out
-// BUILD: clang++ -std=c++14 strpal.cpp -o tmpl.out && tmpl.out
+// BUILD: clang++ -std=c++14 strpal.cpp -Wall -Wextra -o tmpl.out && tmpl.out
 
 #include <iostream>
 #include <chrono>
@@ -18,6 +18,7 @@
 #include <unordered_set>
 #include <vector>
 
+using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -25,17 +26,17 @@ using std::string;
 
 bool is_palindrome(const std::string& ss)
 {
-    int test = 1;
     for (const char *lef = ss.c_str(), *rgt = lef + ss.size(); lef < --rgt; ) {
         printf("\nlef %c ? %c rgt\t", *lef, *rgt);
         if (*lef++ != *rgt) {
-            int test = 2;
             return false;
         }
     }
     return true;
 }
 
+/// const int duh() { return 2; } --> warning: 'const' type qualifier on return type has no effect [-Wignored-qualifiers]
+int duh() { return 2; }
 
 int main() {
 
@@ -43,5 +44,8 @@ int main() {
     for (const string st : strs) {
 	cout << st << "\t=>\t" << is_palindrome(st) << endl;
     }
+    auto ans = duh();
+    ans = 4;
+    cerr << "ans: " << ans << endl;
     return 0;
 }
