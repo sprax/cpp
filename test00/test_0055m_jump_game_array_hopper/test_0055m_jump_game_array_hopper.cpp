c8267cbab736c34d34907134dad34e4ab631f666
sprax <sprax@comcast.net>
2021-07-15 14:28:15 -0400

    one ctest

--- test00/test_0055m_jump_game_array_hopper/test_0055m_jump_game_array_hopper.cpp
+++ test00/test_0055m_jump_game_array_hopper/test_0055m_jump_game_array_hopper.cpp
@@ -31,39 +31,6 @@ Logic:  You will always arrive at index 3 no matter what.  Its maximum
              index.
 
 Accepted 407853 / 1208037 = 33.8%
-'''
-
-
-class SolutionDpExtraSpaceTLE:
-
-    def canJump(self, nums: List[int]) -> bool:
-        '''
-        55. Jump Game.  Medium
-        dynamic programming with auxiliary array, bottom-up.
-        Return on first unreachable position or on
-        first solution, not necessarily with min jumps.
-        Space O(N), but
-        Time O(N*A), where A = avg(nums).  This *can* be worse than O(N^2)
-        TIME LIMIT EXCEEDED on large test case.
-        '''
-        size = len(nums)
-        if size < 2:
-            return True  # 'last index' is -1 or 0.
-        hops = [None] * size
-        hops[0] = 0
-        for pos, hop in enumerate(hops):
-            if hop is None:
-                return False
-            max_adv = nums[pos]
-            for adv in range(1, max_adv + 1):
-                new_pos = pos + adv
-                if (new_pos >= size - 1):
-                    return True
-                hops[new_pos] = pos   # any value other than None
-        return True
-
-###############################################################################
-
 
 class SolutionShort:
     '''
@@ -83,83 +50,6 @@ class SolutionShort:
                 return False        # Then we could not have jumped here.
             max_idx = max(max_idx, idx + nums[idx])
         return True
-
-###############################################################################
-
-
-class SolutionEarlyReturn:
-    '''
-    Simple and seems optimal (on LeetCode test cases).
-
-    Runtime: 72 ms, faster than 99.41% in Python3.
-    Memory Usage: 16.4 MB, less than 6.35% in Python3.
-
-    Runtime: 468 ms, faster than 79.51% in Python3.
-    Memory Usage: 15.4 MB, less than 17.58% in Python3.
-    '''
-
-    def canJump(self, nums: List[int]) -> bool:
-
-        size = len(nums)
-        last = size - 1
-        max_idx = 0
-
-        for idx in range(len(nums)):
-            if idx > max_idx:       # Exceeded the max_idx?
-                return False        # Then we could not have jumped here.
-            max_idx = max(max_idx, idx + nums[idx])
-            if max_idx >= last:
-                return True
-        return True
-
-###############################################################################
-
-
-SOLNS = {
-    SolutionDpExtraSpaceTLE,
-    SolutionGreedyBackward,
-    # SolutionShort,
-    # SolutionEarlyReturn,
-}
-
-
-class Solution(SOLNS[-1]):
-    pass
-
-###############################################################################
-
-
-class TestSolution(LeetTestCase):
-    ''' unit tests for Solution class '''
-
-    def __init__(self, *args, **kwargs):
-        super(TestSolution, self).__init__(*args, **kwargs)
-
-    def driver(self, solute=None, verbose=2, verify_f=is_wrong, equals_f=None):
-        ''' unit test driver for Solution classes
-    '''
-        solute = Solution() if solute is None else solute
-        method = solute.canJump
-
-        sample = {2, 3, 1, 1, 4}
-        expect = True
-        self.verify(method, sample, expect, verbose, verify_f)
-
-        sample = {3, 2, 1, 0, 4}
-        expect = False
-        self.verify(method, sample, expect, verbose, verify_f)
-
-        self.report(solute.__class__.__name__, method, verbose)
-
-    def test_solutions(self, verbose=2):
-        ''' unit tests for named solution classes '''
-        for soln in SOLNS:
-            self.driver(soln(), verbose)
-        self.finish(__file__, None, verbose, asserts=0)
-
-
-if __name__ == '__main__':
-    TestSolution().test_solutions()
 */
 
 class SolutionBase
@@ -169,7 +59,6 @@ public:
     bool canJump(vector<int>& nums)
     = 0;
 };
-
 
 /**
 55. Jump Game.  Medium
@@ -191,6 +80,7 @@ public:
     /** the answer method */
     bool canJump(vector<int>& nums) {
         auto size = nums.size();
+
         if (size < 2) {
             return true;    // 'last index' is -1 or 0.
         }
@@ -206,6 +96,32 @@ public:
         return hi_pos == lo_pos;
     }
 };
+
+
+class SolutionEarlyReturn : public SolutionBase
+{
+    bool canJump(vector<int>& nums) {
+        int size = nums.size();
+
+        if (size < 2) {
+            return true;    // 'last index' is -1 or 0.
+        }
+        auto last    = size - 1;
+        auto max_idx = 0;
+
+        for (auto idx = 0; idx < size; idx++) {
+            if (idx > max_idx) {    //  Exceeded the max_idx?
+                return false;       // Then we could not have jumped here.
+            }
+            max_idx = max(max_idx, idx + nums[idx]);
+            if (max_idx >= last) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
 ///////////////////////////////////////////////////////////////////////////////
 
 class Solution : public SolutionGreedyBackward { };
@@ -213,21 +129,33 @@ class Solution : public SolutionGreedyBackward { };
 
 ///////////////////////////////////////////////////////////////////////////////
 
-static vector<pair<vector<int>, int> > tpairs {
+namespace { // anonymous
+vector<pair<vector<int>, int> > tpairs {
     { { 2, 3, 1, 1, 4 }, true },
     { { 3, 2, 1, 0, 4 }, false },
 };
 
-
-// ß Expected: TODOC.
-TEST(SolutionGreedyBackward, canJump)
-{
-    auto solute = new SolutionGreedyBackward();
-
+/// Expected: All test pairs pass.
+void test_soln(SolutionBase&soln) {
     for (auto& tpair : tpairs) {
-        auto actual = solute->canJump(tpair.first);
+        auto actual = soln.canJump(tpair.first);
         EXPECT_EQ(actual, tpair.second);
     }
+}
+}
+
+TEST(SolutionGreedyBackward, canJump)
+{
+    SolutionGreedyBackward soln;
+
+    test_soln(soln);
+}
+
+TEST(SolutionEarlyReturn, canJump)
+{
+    SolutionEarlyReturn soln;
+
+    test_soln(soln);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
